66910cff4dfc71b27a93b916edbed706bd7b5fe0
Lil-fortune <89183750+Lil-fortune@users.noreply.github.com>
2021-08-26 14:42:23 +0200

    Changes

---
+++ Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿
+namespace Vaccine_Registration
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ID = new System.Windows.Forms.Label();
+            this.IDTextBox = new System.Windows.Forms.TextBox();
+            this.Surname = new System.Windows.Forms.Label();
+            this.SurnameTextBox = new System.Windows.Forms.TextBox();
+            this.Firstname = new System.Windows.Forms.Label();
+            this.FirstnameTextBox = new System.Windows.Forms.TextBox();
+            this.Gender = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ProvinceTextBox = new System.Windows.Forms.TextBox();
+            this.RegisterButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.GenderComboBox = new System.Windows.Forms.ComboBox();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ID
+            // 
+            this.ID.AutoSize = true;
+            this.ID.Location = new System.Drawing.Point(46, 76);
+            this.ID.Name = "ID";
+            this.ID.Size = new System.Drawing.Size(24, 20);
+            this.ID.TabIndex = 0;
+            this.ID.Text = "ID";
+            this.ID.Click += new System.EventHandler(this.ID_Click);
+            // 
+            // IDTextBox
+            // 
+            this.IDTextBox.Location = new System.Drawing.Point(118, 73);
+            this.IDTextBox.Name = "IDTextBox";
+            this.IDTextBox.Size = new System.Drawing.Size(178, 27);
+            this.IDTextBox.TabIndex = 1;
+            this.IDTextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // Surname
+            // 
+            this.Surname.AutoSize = true;
+            this.Surname.Location = new System.Drawing.Point(46, 121);
+            this.Surname.Name = "Surname";
+            this.Surname.Size = new System.Drawing.Size(67, 20);
+            this.Surname.TabIndex = 2;
+            this.Surname.Text = "Surname";
+            this.Surname.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // SurnameTextBox
+            // 
+            this.SurnameTextBox.Location = new System.Drawing.Point(119, 118);
+            this.SurnameTextBox.Name = "SurnameTextBox";
+            this.SurnameTextBox.Size = new System.Drawing.Size(177, 27);
+            this.SurnameTextBox.TabIndex = 3;
+            // 
+            // Firstname
+            // 
+            this.Firstname.AutoSize = true;
+            this.Firstname.Location = new System.Drawing.Point(46, 166);
+            this.Firstname.Name = "Firstname";
+            this.Firstname.Size = new System.Drawing.Size(73, 20);
+            this.Firstname.TabIndex = 4;
+            this.Firstname.Text = "Firstname";
+            this.Firstname.Click += new System.EventHandler(this.Firstname_Click);
+            // 
+            // FirstnameTextBox
+            // 
+            this.FirstnameTextBox.Location = new System.Drawing.Point(119, 166);
+            this.FirstnameTextBox.Name = "FirstnameTextBox";
+            this.FirstnameTextBox.Size = new System.Drawing.Size(177, 27);
+            this.FirstnameTextBox.TabIndex = 5;
+            // 
+            // Gender
+            // 
+            this.Gender.AutoSize = true;
+            this.Gender.Location = new System.Drawing.Point(46, 212);
+            this.Gender.Name = "Gender";
+            this.Gender.Size = new System.Drawing.Size(57, 20);
+            this.Gender.TabIndex = 6;
+            this.Gender.Text = "Gender";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 256);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Province";
+            // 
+            // ProvinceTextBox
+            // 
+            this.ProvinceTextBox.Location = new System.Drawing.Point(119, 256);
+            this.ProvinceTextBox.Name = "ProvinceTextBox";
+            this.ProvinceTextBox.Size = new System.Drawing.Size(177, 27);
+            this.ProvinceTextBox.TabIndex = 9;
+            // 
+            // RegisterButton
+            // 
+            this.RegisterButton.Location = new System.Drawing.Point(335, 73);
+            this.RegisterButton.Name = "RegisterButton";
+            this.RegisterButton.Size = new System.Drawing.Size(123, 47);
+            this.RegisterButton.TabIndex = 10;
+            this.RegisterButton.Text = "Register";
+            this.RegisterButton.UseVisualStyleBackColor = true;
+            this.RegisterButton.Click += new System.EventHandler(this.RegisterButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(46, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(255, 38);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Vaccine Registation";
+            // 
+            // GenderComboBox
+            // 
+            this.GenderComboBox.FormattingEnabled = true;
+            this.GenderComboBox.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.GenderComboBox.Location = new System.Drawing.Point(119, 209);
+            this.GenderComboBox.Name = "GenderComboBox";
+            this.GenderComboBox.Size = new System.Drawing.Size(177, 28);
+            this.GenderComboBox.TabIndex = 12;
+            this.GenderComboBox.SelectedIndexChanged += new System.EventHandler(this.GenderComboBox_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 345);
+            this.Controls.Add(this.GenderComboBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.RegisterButton);
+            this.Controls.Add(this.ProvinceTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Gender);
+            this.Controls.Add(this.FirstnameTextBox);
+            this.Controls.Add(this.Firstname);
+            this.Controls.Add(this.SurnameTextBox);
+            this.Controls.Add(this.Surname);
+            this.Controls.Add(this.IDTextBox);
+            this.Controls.Add(this.ID);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ID;
+        private System.Windows.Forms.TextBox IDTextBox;
+        private System.Windows.Forms.Label Surname;
+        private System.Windows.Forms.TextBox SurnameTextBox;
+        private System.Windows.Forms.Label Firstname;
+        private System.Windows.Forms.TextBox FirstnameTextBox;
+        private System.Windows.Forms.Label Gender;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ProvinceTextBox;
+        private System.Windows.Forms.Button RegisterButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox GenderComboBox;
+        public System.Windows.Forms.BindingSource bindingSource1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Sql;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Vaccine_Registration
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void RegisterButton_Click(object sender, EventArgs e)
+         
+        {
+            try
+            {
+                string connetionString;
+                SqlConnection cnn;
+
+                connetionString = @"Data Source=DESKTOP-8O3T91G;Initial Catalog=VaccineRegistation;User ID=sa;Password=demol23";
+                String query = "select * from data";
+
+                SqlConnection con = new SqlConnection(str);
+
+                SqlCommand cmd = new SqlCommand(query, con);
+
+                con.Open();
+
+                DataSet ds = new DataSet();
+
+                MessageBox.Show("connect with sql server");
+
+                con.Close();
+
+            }
+
+            catch (Exception es)
+
+            {
+
+                MessageBox.Show(es.Message);
+
+
+
+            }
+
+        }
+    
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Firstname_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ID_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GenderComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
